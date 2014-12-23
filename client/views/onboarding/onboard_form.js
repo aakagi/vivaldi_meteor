f21e0a79e2816db3e20e368cbfba3340feb2ccc5
aakagi <aakagi@purdue.edu>
2014-12-23 16:28:01 -0500

    onboard form updated

--- client/views/onboarding/onboard_form.js
+++ client/views/onboarding/onboard_form.js
@@ -11,24 +11,45 @@ Accounts.onEnrollmentLink(function(token, success) {
 Template.onboard_form.helpers({
     instrumentList: function () {
         return Instrument.find();
+    },
+    teacherSignUp: function () {
+        return Session.get("teacherSignUp");
     }
+
 
 });
 
 Template.onboard_form.events({
+    // Onboard_form submit data to user
     'submit #onboardForm': function(e, t) {
         e.preventDefault();
 
         var onboardForm = $(e.currentTarget),
-            instrument = onboardForm.find('#selectInstrument').val(),
+            tokenVar = Session.get('tokenVar'),
             password = onboardForm.find('#createPassword').val(),
-            passwordConfirm = onboardForm.find('#confirmPassword').val(),
-            tokenVar = Session.get('tokenVar');
+            passwordConfirm = onboardForm.find('#confirmPassword').val();
+        // If user is signing up as a teacher
+        if (teacherSignUp) {
+                var school = onboardForm.find('#school').val(),
+                    honorific = onboardForm.find('#honorific').val(),
+                    lastName = onboardForm.find('#lastName').val();
 
-        console.log(instrument);
+                console.log(school);
+                console.log(honorific);
+                console.log(lastName);
+        } else {
+            var instrument = onboardForm.find('#selectInstrument').val(),
+                firstName = onboardForm.find('#lastName').val(),
+                lastName = onboardForm.find('#lastName').val();
+
+                console.log(instrument);
+                console.log(firstName);
+                console.log(lastName);
+        }
+
+        console.log(tokenVar);
         console.log(password);
         console.log(passwordConfirm);
-        console.log(tokenVar);
 
         if (isNotEmpty(password) && isNotEmpty(instrument) && areValidPasswords(password, passwordConfirm)) {
             Accounts.resetPassword(tokenVar, password, function(err, success) {
@@ -45,4 +66,7 @@ Template.onboard_form.events({
             }); 
         }
     },
+    'change .teacher-sign-up input': function (event) {
+        Session.set("teacherSignUp", event.target.checked);
+    }
 });
