5bfd1dd24bea242ef93208aa1456a86ae1c4145f
Edward Williams <edwardwilliams@edwards-mbp.attlocal.net>
2014-12-27 13:35:16 -0500

    new teacher checking

---
+++ client/views/settings_fragments/create_class.js
@@ -0,0 +1,8 @@
+Template.create_class.events({
+	'click #createClass': function(){
+		//get class data from fields
+		className = document.getElementById('className').value
+		sectionNames = document.getElementById('sectionNames').value
+		//split string using handy-dandy split function
+	}
+})
