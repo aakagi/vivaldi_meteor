21e0799ac4a274a6d5c14b8ea11d3ff08cf3c898
Edward Williams <edwardwilliams@Edwards-MacBook-Pro.local>
2014-12-30 13:52:57 -0500

    students can join sections

--- client/views/settings/main/join_section.js
+++ client/views/settings/main/join_section.js
@@ -1,8 +1,49 @@
 Template.join_section.helpers({
-    sectionList: function () {
-        var classData = Template.currentData();//template initalized with class data
+    sectionList: function() {
+        var classData = Template.currentData(); //template initalized with class data
         var classId = classData._id;
-        return classSectionsById(classId);
+        //get all sections that a student is not a member of and belong to this class
+        selector = {
+            $and: [{
+                users: {
+                    $not: {
+                        $elemMatch: {
+                            $in: [Meteor.userId()]
+                        }
+                    }
+                }
+            }, {
+                _id: {
+                    $in: classData.sections
+                }
+            }, {
+                name: {
+                    $not: 'Teachers'
+                }
+            }]
+        };
+        pointer = Sections.find(selector);
+        return pointer.fetch();
     }
 });
 
+Template.join_section.events({
+    'click #joinSection': function() {
+        var sectionID = document.getElementById('selectSection').value;
+        console.log(sectionID);
+        Sections.update({
+            _id: sectionID
+        }, {
+            $push: {
+                users: Meteor.userId()
+            }
+        }, function(err) {
+            if (err) {
+                console.log(err);
+                setAlert('error', 'error writing to database');
+            } else {
+                setAlert('info', 'added to sections!');
+            }
+        });
+    }
+})
