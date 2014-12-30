dc9d4aeec30a9423577402f7619e6e19c2442027
Edward Williams <edwardwilliams@Edwards-MacBook-Pro.local>
2014-12-30 15:11:19 -0500

    more fun with buttons

--- client/views/home/home_view.js
+++ client/views/home/home_view.js
@@ -9,24 +9,48 @@ Template.home_view.helpers({
     },
     allOtherClasses: function(){
     	return Session.get('allOtherClasses');
+    },
+    showTasks: function(){
+    	return Session.get('showTasks');
+    }, 
+    completedTasks: function(){
+    	return Session.get('completedTasks');
+    },
+    practice: function(){
+    	return Session.get('practice');
     }
 });
 
 Template.home_view.events({
 	'click #showClasses': function(){
-		Session.set('showClasses', true);
+		Session.set('showClasses', false);
 		Session.set('newClass', false);
-		Session.set('allOtherClasses', false)
+		Session.set('allOtherClasses', false);
 	},
 	'click #newClass': function(){
-		Session.set('showClasses', false);
+		Session.set('showClasses', true);
 		Session.set('newClass', true);
-		Session.set('allOtherClasses', false)
+		Session.set('allOtherClasses', false);
 	},
 	'click #allOtherClass': function(){
-		Session.set('showClasses', false);
+		Session.set('showClasses', true);
 		Session.set('newClass', false);
-		Session.set('allOtherClasses', true)
+		Session.set('allOtherClasses', true);
+	},
+	'click #showTasks': function(){
+		Session.set('showTasks', true);
+		Session.set('completedTasks', false);
+		Session.set('practice', false);
+	},
+	'click #completedTasks': function(){
+		Session.set('showTasks', false);
+		Session.set('completedTasks', true);
+		Session.set('practice', false);
+	},
+	'click #practice': function(){
+		Session.set('showTasks', false);
+		Session.set('completedTasks', false);
+		Session.set('practice', true);
 	},
 })
 
