41d8b2852375e20723117509b57ce823b0b9c0eb
aakagi <aakagi@purdue.edu>
2015-01-02 02:07:30 -0500

    user view built but still not working

---
+++ client/views/user/user_view.js
@@ -0,0 +1,45 @@
+Template.user_view.helpers({
+    userClasses: userClasses,
+    isTeacher: isTeacher,
+    // showClasses: function(){
+    //     return Session.get('showClasses');
+    // }, 
+    // allOtherClasses: function(){
+    //     return Session.get('allOtherClasses');
+    // },
+    // showTasks: function(){
+    //     return Session.get('showTasks');
+    // }, 
+    // completedTasks: function(){
+    //     return Session.get('completedTasks');
+    // },
+    // practice: function(){
+    //     return Session.get('practice');
+    // }
+});
+
+Template.user_view.events({
+    // 'click #showClasses': function(){
+    //     Session.set('showClasses', false);
+    //     Session.set('allOtherClasses', false);
+    // },
+    // 'click #allOtherClass': function(){
+    //     Session.set('showClasses', true);
+    //     Session.set('allOtherClasses', true);
+    // },
+    // 'click #showTasks': function(){
+    //     Session.set('showTasks', true);
+    //     Session.set('completedTasks', false);
+    //     Session.set('practice', false);
+    // },
+    // 'click #completedTasks': function(){
+    //     Session.set('showTasks', false);
+    //     Session.set('completedTasks', true);
+    //     Session.set('practice', false);
+    // },
+    // 'click #practice': function(){
+    //     Session.set('showTasks', false);
+    //     Session.set('completedTasks', false);
+    //     Session.set('practice', true);
+    // },
+})
