7b4c6196d9627fc8441401b4b9f3aea32841cdee
aakagi <aakagi@purdue.edu>
2015-01-03 01:24:50 -0500

    small tweaks

--- client/views/class/main/class_task_list_main.js
+++ client/views/class/main/class_task_list_main.js
@@ -1,3 +1,34 @@
+Template.teacher_task.helpers({
+    isPracticeTask: function(){
+        return Template.currentData().type == 'Practice';
+    },
+    isPracticeTask: function(){
+        return Template.currentData().type == 'Audio';
+    },
+    formatDate: function(){
+        //simple for now, could do better formatting later
+        var date = Template.currentData().dueDate;
+        return date.toDateString()
+    },
+    className: function(){
+        var classData = Template.parentData(1);
+        return classData.name;
+    },
+    sectionNames: function(){
+        var taskData = Template.currentData();
+        return Sections.find({_id: {$in: taskData.sections}}).fetch();
+    }
+});
+
+Template.teacher_task.events({
+    'click #delete': function(){
+        //must delete the task and all instances of taskData
+        var taskID = Template.currentData()._id;
+        Meteor.call('removeTask', taskID);
+    }
+});
+
+
 Template.class_task_list_main.helpers({
 	getTasks: function(){
 		var classId = Template.currentData()._id;
@@ -5,3 +36,5 @@ Template.class_task_list_main.helpers({
 		return pointer.fetch();
 	}
 })
+
+
