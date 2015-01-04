74a66ff6de280704ea3e075d6a48ac1bb05852aa
Edward Williams <edwardwilliams@edwards-air.attlocal.net>
2015-01-04 17:59:24 -0500

    section management

--- client/views/class/main/manage_class.js
+++ client/views/class/main/manage_class.js
@@ -35,11 +35,11 @@ Template.manage_class.events({
             Router.go('home')
         } else {
             console.log("Action Cancelled")
-        }   
+        }
     }
 });
 
-Template.manage_class.rendered = function () {
+Template.manage_class.rendered = function() {
     Session.set('saveSections', true);
     Session.set('editSections', false);
 
@@ -90,7 +90,7 @@ Template.manage_class.helpers({
     'editSections': function() {
         return Session.get('editSections');
     },
-    'confirmAddedSection': function()  {
+    'confirmAddedSection': function() {
         return Session.get('confirmAddedSection');
     },
     'addNewSection': function() {
@@ -132,30 +132,100 @@ Template.manage_class.events({
             // Delete Class
         } else {
             console.log("Action Cancelled")
-        }   
+        }
     },
     'click #saveSections': function() {
+        //save all changes
+
+        var classData = Template.currentData();
+
+        for (indx in classData.sections) {
+            console.log(indx);
+            //get all new fields
+            var sectionID = classData.sections[indx];
+            var sectionData = Sections.findOne({
+                _id: sectionID
+            });
+            if (document.getElementById(sectionID + "name")) { //if this is false, then the section is not visible
+                var newName = document.getElementById(sectionID + "name").value;
+                var newLeader = document.getElementById(sectionID + "leader").value;
+                var newOrder = document.getElementById(sectionID + "order").value;
+                if (newName != sectionData.name) {
+                    //new name
+                    Sections.update({
+                        _id: sectionID
+                    }, {
+                        $set: {
+                            name: newName
+                        }
+                    });
+                }
+
+                if (newLeader != 'noleader' && newLeader != sectionData.leader) {
+                    console.log("saving new leader");
+                    //save new leader
+                    Sections.update({
+                        _id: sectionID
+                    }, {
+                        $set: {
+                            leader: newLeader
+                        }
+                    });
+                }
+                if (newOrder) {
+                    //save new order
+                    Sections.update({
+                        _id: sectionID
+                    }, {
+                        $set: {
+                            order: newOrder
+                        }
+                    });
+                }
+            }
+
+        }
         Session.set('saveSections', true);
         Session.set('editSections', false);
+
     },
     'click #editSections': function() {
         Session.set('saveSections', false);
         Session.set('editSections', true);
     },
-    'click #confirmAddedSection': function()  {
+    'click #confirmAddedSection': function() {
+        //get the new section name: 
+        var classID = Template.currentData()._id;
+        console.log("confirming");
+        var sectionName = document.getElementById('newAddedSection').value
+        console.log(sectionName);
+        if (sectionName){
+            //get the class data
+            var classData = Template.currentData();
+            var newSectionDoc = {
+                    name: sectionName,
+                    users: [],
+                    order: classData.sections.length
+                };
+            var sectionID = Sections.insert(newSectionDoc);
+            Classes.update({_id: classID}, {$push:{sections: sectionID}});
+        }
         Session.set('confirmAddedSection', true);
         Session.set('addNewSection', false);
     },
     'click #addNewSection': function() {
         Session.set('confirmAddedSection', false);
         Session.set('addNewSection', true);
+
+       
+        
     },
 });
 
 Template.waitlist_students.events({
     'click #confirmStudent': function() {
         var studentID = Template.currentData()._id;
-        var classData = Template.parentData(1); 
+        var classData = Template.parentData(1);
         var waitList = classData.waitlist;
         var studentList = classData.students;
         var indx = waitList.indexOf(studentID);
@@ -211,7 +281,7 @@ Template.class_students.events({
 });
 
 Template.class_sections.events({
-    'click #deleteSection': function(){
+    'click #deleteSection': function() {
         if (confirm("Are you sure you want to delete this section?") == true) {
             var sectionID = Template.currentData()._id;
             var classData = Template.parentData(1);
@@ -222,5 +292,53 @@ Template.class_sections.events({
         } else {
             console.log("Action Cancelled")
         }
+    }
+});
+
+Template.edit_sections.helpers({
+    'click #deleteSection': function() {
+        if (confirm("Are you sure you want to delete this section?") == true) {
+            var sectionID = Template.currentData()._id;
+            var classData = Template.parentData(1);
+            var sectionList = classData.sections;
+            var indx = sectionList.indexOf(sectionID);
+            // Removes from class array
+            Meteor.call("deleteSection", sectionID, classData._id);
+        } else {
+            console.log("Action Cancelled")
+        }
+    }
+});
+
+Template.edit_sections.helpers({
+    'currentLeader': function() {
+        if (Template.currentData().leader) {
+            var result = Meteor.users.findOne({
+                _id: Template.currentData().leader
+            });
+            return result;
+        } else {
+            return {
+                profile: {
+                    firstName: "No",
+                    lastName: "Leader"
+                },
+                _id: "noleader"
+            }
+        }
+    },
+    'sectionStudentsNotLeader': function() {
+        console.log("test");
+        return Meteor.users.find({
+            $and: [{
+                _id: {
+                    $in: Template.currentData().users
+                }
+            }, {
+                _id: {
+                    $ne: Template.currentData().leader
+                }
+            }]
+        }).fetch();
     }
 });
