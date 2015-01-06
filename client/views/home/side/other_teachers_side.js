781813c5ca0cdfb8378c0bcc8e3844801569a466
aakagi <aakagi@purdue.edu>
2015-01-06 14:37:43 -0500

    half way done with other teachers side

--- client/views/home/side/other_teachers_side.js
+++ client/views/home/side/other_teachers_side.js
@@ -3,3 +3,22 @@ Template.other_teachers_side.helpers({
         // return otherTeacherDocuments;
     }
 }); 
+
+Template.other_teachers_side.rendered = function() {
+    $('#otherTeachersList').css('display', 'none');
+
+    $('#previewOtherTeachers').click(function(event) {
+        $('#previewOtherTeachers').slideUp(250);
+        $('#otherTeachersList').slideDown(250);
+    });
+
+    $('#cancel').click(function(event) {
+        $('#classCreateForm').slideUp(250);
+        $('#otherTeachersList').slideDown(250);
+    });
+    
+    // $('#createClass').click(function(event) {
+    //     $('#classCreateForm').slideUp(250);
+    //     $('#previewCreateClass').slideDown(250);
+    // }); 
+}
