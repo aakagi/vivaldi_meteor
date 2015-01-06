7c9f1dd8d06d102d9971ade21dcc0020730db9a6
Edward Williams <edwardwilliams@Edwards-MacBook-Pro.local>
2015-01-06 16:14:16 -0500

    naming conflict fixed

--- client/views/user/main/user_messenger_main.js
+++ client/views/user/main/user_messenger_main.js
@@ -4,32 +4,44 @@ Template.messenger.rendered = function() {
 Template.user_messenger_main.helpers({
     getMessages: function() {
         //gets all messages between the two users
-        var selector1 = {senderID: Template.currentData()._id, recipientID: Meteor.userId()};
-        var selector2 = {senderID: Meteor.userId(), recipientID: Template.currentData()._id};
-        var selector = {$or: [selector1, selector2]};
-        var pointer = userMessages.find(selector, {sort: {date: 1}});
+        var selector1 = {
+            senderID: Template.currentData()._id,
+            recipientID: Meteor.userId()
+        };
+        var selector2 = {
+            senderID: Meteor.userId(),
+            recipientID: Template.currentData()._id
+        };
+        var selector = {
+            $or: [selector1, selector2]
+        };
+        var pointer = userMessages.find(selector, {
+            sort: {
+                date: 1
+            }
+        });
         return pointer.fetch();
     },
 });
 
-sendMessage = function() {
-        var messagebody = document.getElementById("new-message").value;
-        if (messagebody) {
-            var newMessage = {
-                recipientID: Template.currentData()._id,
-                senderID: Meteor.userId(),
-                body: messagebody,
-                date: new Date()
-            };
-            console.log(newMessage)
-            userMessages.insert(newMessage);
-            document.getElementById("new-message").value = "";
-            
-        }
+sendMessageUser = function() {
+    var messagebody = document.getElementById("new-message").value;
+    if (messagebody) {
+        var newMessage = {
+            recipientID: Template.currentData()._id,
+            senderID: Meteor.userId(),
+            body: messagebody,
+            date: new Date()
+        };
+        console.log(newMessage)
+        userMessages.insert(newMessage);
+        document.getElementById("new-message").value = "";
+
     }
+}
 
 Template.user_messenger_main.events({
-    'click #new-message-submit': sendMessage,
+    'click #new-message-submit': sendMessageUser,
     'keydown input': function(e) {
         if (e.keyCode == 13) {
             //send message
@@ -41,12 +53,13 @@ Template.user_messenger_main.events({
 Template.message_user.helpers({
     senderName: function() {
         var senderID = Template.currentData().senderID;
-        if (senderID == Meteor.userId()){
+        if (senderID == Meteor.userId()) {
             return "Me"
-        }
-        else{
+        } else {
             //possibly unsecure 
-            var sender = Meteor.users.findOne({_id: senderID}).profile;
+            var sender = Meteor.users.findOne({
+                _id: senderID
+            }).profile;
             $('.messages').scrollTop(100000);
             return sender.firstName + " " + sender.lastName;
         }
