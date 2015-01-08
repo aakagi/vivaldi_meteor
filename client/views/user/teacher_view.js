46a4b5c7a5bef5cb3a5d01afdf4ab78917e8ec9c
Edward Williams <edwardwilliams@Edwards-MacBook-Air.local>
2015-01-08 17:12:34 -0500

    teacher chat

--- client/views/user/teacher_view.js
+++ client/views/user/teacher_view.js
@@ -6,23 +6,31 @@ Template.teacher_view.helpers({
     //     return true;
     // },
     usersInSameClass: function() {
-        // TODO because I can't figure it out, god damn it
-        // var myClasses = userClasses();
-        // // Make sure userClassesById works!
-        // var profileClasses = userClassesById(Template.currentData()._id);
+        //want to know if this teacher is the teacher of one of your classes
 
+        var teacherID = Template.currentData()._id;
 
+        //get all classes where a this teacher is a teacher and this student is a student
 
-        // if (classTeachers.indexOf(myId) > -1 || classStudents.indexOf(myId) > -1) {
-        //     console.log('true');
-        //     return true;
-        // } else {
-        //     console.log('false');
-        //     return false;
-        // }
+        var classes = Classes.find({
+            $and: [{
+                teachers: {
+                    $elemMatch: {
+                        $in: [teacherID]
+                    }
+                }
+            }, {
+                students: {
+                    $elemMatch: {
+                        $in: [Meteor.userId()]
+                    }
+                }
+            }]
+        }).fetch();
+        return classes.length > 0;
     },
     unrelatedStudent: function() {
-        return !isTeacher  && !studentsInSameClass(myID, thisID);
+        return !isTeacher && !studentsInSameClass(myID, thisID);
     },
     userMessage: function() {
         return Session.get('userMessage');
