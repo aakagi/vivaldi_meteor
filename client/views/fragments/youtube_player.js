16c1fa21cee43199e56eb7de34133d8a0e6f4637
Edward Williams <edwardwilliams@Edwards-MacBook-Air.local>
2015-01-09 18:10:22 -0500

    video task completion almost"

--- client/views/fragments/youtube_player.js
+++ client/views/fragments/youtube_player.js
@@ -6,6 +6,9 @@ Template.youtube_player.rendered = function() {
 	var firstScriptTag = document.getElementsByTagName('script')[0];
 	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+	//disable completion button
+	$('#completeTask').addClass('disabled').attr('disabled', 'disabled');
+
 	onYouTubeIframeAPIReady();
 }
 
@@ -15,8 +18,19 @@ Template.youtube_player.helpers({
 	}
 });
 
+enableCompletion = function(){
+	$('#getTask').removeClass('disabled').removeAttr('disabled', 'disabled');
+}
+
 Template.youtube_player.events({
 	'click .goBackFromListen': function() {
 		Session.set('showVideo', false);
+	},
+	'click #completeTask': function(){
+		console.log("complete task");
+		var listenTask = Session.get('listenTaskObject');
+
+		Meteor.call('completeTask', Meteor.userId(), listenTask._id);
+		setAlert('info', 'task completed!');
 	}
 });
